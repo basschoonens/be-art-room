b6484ca99570a15c523b0e18b63896f15f10cec6
basschoonens <bas.schoonens@gmail.com>
2024-03-10 15:08:37 +0100

    start eindopdracht

---
+++ src/main/java/nl/novi/theartroom/TheArtRoomApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.theartroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheArtRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheArtRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/theartroom/TheArtRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.theartroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheArtRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
